618bf213a0ff4e777136e8e78bc6a1e6b1619be1
matthewgreer <matthew.greer.dev@gmail.com>
2020-07-29 16:06:00 -0400

    Restored working auth. Restarted styling. Doh.

--- frontend/components/banner/banner.jsx
+++ frontend/components/banner/banner.jsx
@@ -4,21 +4,33 @@ import { Link } from 'react-router-dom';
 const Banner = ({ currentUser, logout }) => {
 
   const subscribeLogin = () => (
-    <nav className="subscribe-login">
+    <nav className="user-nav-button-container">
       <Link to="/subscribe" className="subscribe-button">Subscribe</Link>
       <Link to="/login" className="login-button">Log In</Link>
     </nav>
   );
 
   const userLogout = () => (
-    <nav className="user-logout">
+    <nav className="user-nav-button-container">
       <h2>Current User: {currentUser.email} (for test purposes only)</h2>
       <button className="logout-button" onClick={logout}>Log Out</button>
     </nav>
   );
 
-  return currentUser ? userLogout() : subscribeLogin();
-
+  return(
+    <div>
+      <div>
+        
+      </div>
+      <div>
+        <a href="/" class="page-header-logo-link">
+          Logo goes here!
+        </a>
+      </div>
+      <div>
+        {currentUser ? userLogout() : subscribeLogin()}
+      </div>
+    </div>)
 }
 
 export default Banner;
