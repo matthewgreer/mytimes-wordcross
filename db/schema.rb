6ea523fe8745890c196556ea63aa202f0449fa08
matthewgreer <matthew.greer.dev@gmail.com>
2020-10-21 13:59:49 -0400

    Add timezone to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_21_144437) do
+ActiveRecord::Schema.define(version: 2020_10_21_154534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_10_21_144437) do
     t.string "leaderboard_url_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "timezone"
   end
 
 end
