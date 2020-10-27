7ef5761c6717ee00a07af2f45bdb55b3950ab329
matthewgreer <matthew.greer.dev@gmail.com>
2020-10-27 11:05:33 -0400

    Comment unused Micro controller

--- app/controllers/api/micros_controller.rb
+++ app/controllers/api/micros_controller.rb
@@ -1,30 +1,25 @@
 class Api::MicrosController < ApplicationController
 
-  before_action :require_logged_in!
+  # before_action :require_logged_in!
 
-  def show
+  # def fetch
 
-    # find Micro by id
-    if params[:id]
-      @micro = Micro.find_by(micro: params[:id])
-      # OR find Micro by date
-    elsif params[:date]
-      @micro = Micro.find_by(micro: params[:date])
-    end
+  #   # find Micro by id
+  #   if params[:id]
+  #     @micro = Micro.find_by(micro: params[:id])
+  #     # OR find Micro by date
+  #   elsif params[:date]
+  #     @micro = Micro.find_by(micro: params[:date])
+  #   end
 
-    if @micro
-      # render to JSON
-      render json: @micro
-    else
-      errors = @micro.errors.full_messages
-      render json: errors, status: 401
-    end
-  end
+  #   if @micro
+  #     # render to JSON
+  #     render json: @micro
+  #   else
+  #     errors = @micro.errors.full_messages
+  #     render json: errors, status: 401
+  #   end
 
-  private
-
-  def micro_params
-    params.require(:micro).permit(:id, :date)
-  end
+  # end
 
 end
