6fde78f5e9a23f28d5c05d1be46795354f1ee7e6
matthewgreer <matthew.greer.dev@gmail.com>
2020-10-27 15:05:31 -0400

    Create protected route for Micro

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -7,19 +7,29 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => (
   <Route
     path={path}
     exact={exact}
-    render={props =>
-      !loggedIn ? <Component {...props} /> : <Redirect to="/" />
-    }
-  />
+    render={props => (
+      !loggedIn ? (
+        <Component {...props} />
+        ) : (
+        <Redirect to="/" />
+        )
+    )} />
+);
+
+const Protected = ({ component: Component, path, loggedIn, exact }) => (
+  <Route path={path} exact={exact} render={(props) => (
+    loggedIn ? (
+    <Component {...props} />
+    ) : (
+    <Redirect to="/login" />
+    )
+  )} />
 );
 
 const msp = (state) => {
   return { loggedIn: Boolean(state.session.id) };
 };
 
-export const AuthRoute = withRouter(
-  connect(
-    msp,
-    null
-  )(Auth)
-);
+export const AuthRoute = withRouter(connect(msp, null)(Auth));
+
+export const ProtectedRoute = withRouter(connect(msp, null)(Protected));
