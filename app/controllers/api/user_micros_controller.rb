e494a03ba70f25c4045b45b53d514421154ffd6d
matthewgreer <matthew.greer.dev@gmail.com>
2020-10-27 12:27:58 -0400

    Debug user_micros_controller

--- app/controllers/api/user_micros_controller.rb
+++ app/controllers/api/user_micros_controller.rb
@@ -30,11 +30,10 @@ class Api::UserMicrosController < ApplicationController
       )
       @user_micro.init_grid_state(@micro.solution)
       @user_micro.puzzle_date = @micro.puzzle_date
-      @user_micro.save
     end
 
-    # send user_micro and micro data as JSON
-    if @user_micro
+    # commit to db and render user_micro and micro data to frontend as JSON
+    if @user_micro.save
       response = {:user_micro => @user_micro, :micro => @micro}
       render json: response
     else
@@ -45,16 +44,15 @@ class Api::UserMicrosController < ApplicationController
   end
 
   def update
+    debugger
     # query for user_micro by id
     @user_micro = UserMicro.find(params[:id])
     
-    # update solving_state && timer && solved
+    # update solving_state
     @user_micro.solving_state = params[:user_micro][:solving_state]
-    @user_micro.timer = params[:user_micro][:timer]
-    @user_micro.solved = params[:user_micro][:solved]
 
-    # save to db
-    if @user_micro.save
+    # save to db (with updated timer, solved status)
+    if @user_micro.update(user_micro_params)
       render json: @user_micro
     else
       errors = @user_micro.errors.full_messages
@@ -62,10 +60,18 @@ class Api::UserMicrosController < ApplicationController
     end
   end
 
-  # private
+  private
 
-  # def user_micro_params
-  #   params.require(:user_micro).permit(:id, :user_id, :puzzle_date)
-  # end
+  def user_micro_params
+    debugger
+    params.require(:user_micro).permit(
+      :id,
+      :user_id,
+      :puzzle_date,
+      :solving_state,
+      :timer,
+      :solved
+    )
+  end
 
 end
