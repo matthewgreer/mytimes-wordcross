84ed9adcab657e6a40c10eb2c5c2315f78dc6164
matthewgreer <matthew.greer.dev@gmail.com>
2020-10-28 15:36:20 -0400

    Add mdp to micro_container

--- frontend/components/body/micro_container.js
+++ frontend/components/body/micro_container.js
@@ -1,10 +1,17 @@
 import { connect } from "react-redux";
+import { fetchMicro } from '../../actions/micro_actions'
+import { updateMicro } from "../../util/micro_api_util";
 import Micro from "./micro";
 
 const msp = (state) => ({
   user_micro: state.entities.micros.user_micro,
 });
 
-const MicroContainer = connect(msp, null)(Micro);
+const mdp = dispatch => ({
+  fetchMicro: (user, puzzle_date) => dispatch(fetchMicro(user, puzzle_date)),
+  updateMicro: user_micro => dispatch(updateMicro(user_micro))
+});
+
+const MicroContainer = connect(msp, mdp)(Micro);
 
 export default MicroContainer;
