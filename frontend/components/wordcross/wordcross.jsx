f1037af54b6dca1519ba505cebafa590c69ecf24
matthewgreer <matthew.greer.dev@gmail.com>
2020-11-02 14:45:51 -0500

    Sketch function of wordcross component in comments

---
+++ frontend/components/wordcross/wordcross.jsx
@@ -0,0 +1,7 @@
+// This component should render the following child components with
+  // their necessary props:
+    // Modal
+    // PuzzleHeader
+    // CurrentClue
+    // PuzzleGrid
+    // ClueList
---
+++ frontend/components/wordcross/wordcross_container.js
@@ -0,0 +1,4 @@
+// This container should:
+  // map the user_micro or user_daily slice of state to its 
+    // child components' props.
+  // map fetch actions? to props??? 
