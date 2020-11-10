0663eb78f2a1a9b498f3f831cf50fcd05d54236b
matthewgreer <matthew.greer.dev@gmail.com>
2020-11-10 11:30:17 -0500

    Render grid correctly with inputs updating single state

--- frontend/components/wordcross/wordcross_board/wordcross_board.jsx
+++ frontend/components/wordcross/wordcross_board/wordcross_board.jsx
@@ -8,20 +8,12 @@ class WordcrossBoard extends React.Component {
     this.state = {
       solvingDirection: "across",
       boxInFocus: "0,0",
-      // when can I set this.state.board to the solving_state in props
-        // componentDidMount?, componentDidUpdate?
     };
   
     this.createBoard = this.createBoard.bind(this);
     this.updateBoard = this.updateBoard.bind(this);
 
   };
-
-  // componentDidMount() {
-  //   if (this.props.data.solving_state) {
-  //     this.createBoard()
-  //   }
-  // };
 
   componentDidUpdate() {
     if (!this.state.board && this.props.data.solving_state) {
@@ -30,32 +22,8 @@ class WordcrossBoard extends React.Component {
   };
 
   createBoard() {
-    const newBoard = []
-    this.props.data.solving_state.map((row, rowIdx) => {
-      newBoard.push([]);
-      row.map((boxValue, boxIdx) => {
-        let position = [rowIdx, boxIdx];
-        newBoard[rowIdx].push(
-          <Box
-            position={position}
-            key={position.toString()}
-            isInFocus={false}
-            updateBoard={this.updateBoard}
-            value={boxValue}
-          />
-          // new Box({
-          //   position: position,
-          //   key: position.toString(),
-          //   value: boxValue,
-          //   isInFocus: false,
-          //   updateBoard: this.updateBoard
-          // })
-        )
-      });
-    });
-    debugger
     return this.setState({
-      board: newBoard
+      board: Object.assign([], this.props.data.solving_state)
     });
   };
 
@@ -64,6 +32,7 @@ class WordcrossBoard extends React.Component {
     const row = position[0];
     const col = position[1];
     updatedBoard[row][col] = newValue;
+    debugger
     return this.setState({
       board: updatedBoard
     });
@@ -79,38 +48,23 @@ class WordcrossBoard extends React.Component {
               key={"row" + rowIdx.toString()}
               className="wordcross-row"
             >
-            <>
-              {row.map((box) => {
-                return box
-              })}
-            </>
+            {row.map((boxValue, boxIdx) => {
+              let position = [rowIdx, boxIdx];
+              return (
+                <Box
+                  position={position}
+                  key={position.toString()}
+                  isInFocus={false}
+                  updateBoard={this.updateBoard}
+                  value={boxValue}
+                />  
+              )
+            })}
           </div>)
-        })}
-        {/* {this.state.board && <table className="wordcross-grid">
-          <tbody>
-            {
-              this.state.board.map((row, rowIdx) => {
-                return <tr key={"row" + rowIdx.toString()}>
-                  {row.map((boxValue, boxIdx) => {
-                    let position = [rowIdx, boxIdx];
-                    return <Box
-                      position={position}
-                      key={position.toString()}
-                      value={boxValue}
-                      isInFocus={false}
-                      update={this.updateBoard}
-                    />
-                  })}
-                </tr>
-              })
-            }
-          </tbody>
-        </table>} */}
-        
+        })}   
       </section>
     )
   }
-    
 }
 
 export default WordcrossBoard;
