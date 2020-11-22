dcd1eaa5dbb22956f2b65ec1ec65da299ca9d6f9
matthewgreer <matthew.greer.dev@gmail.com>
2020-11-22 15:51:18 -0500

    Fix user_micro api data literal parentheses

--- frontend/util/user_micro_api_util.js
+++ frontend/util/user_micro_api_util.js
@@ -3,8 +3,10 @@ export const fetchUserMicro = (user_id, wordcross_date) => $.ajax({
   url: `/api/users/${user_id}/user_micros/${wordcross_date}`,
 });
 
-export const updateUserMicro = (user_micro) => $.ajax({
+export const updateUserMicro = (user_micro) => {
+  $.ajax({
   method: 'PATCH',
   url: `/api/users/${user_micro.user_id}/user_micros/${user_micro.id}`,
-  data: { user_micro }
-});
+  data: ({ user_micro })
+  })
+};
