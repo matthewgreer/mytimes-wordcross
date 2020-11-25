233506ac654c0679103c859d74d4e939d7e79730
matthewgreer <matthew.greer.dev@gmail.com>
2020-11-25 18:49:18 -0500

    Add dailyType to Body

--- frontend/components/body/body.jsx
+++ frontend/components/body/body.jsx
@@ -9,9 +9,9 @@ class Body extends React.Component {
     
     // get current date and time
     // !!! TO DO: eventually have it update regularly
-    const todaysDate = new Date();
+    this.todaysDate = new Date();
 
-    this.todaysFullDate = todaysDate.toLocaleDateString(
+    this.todaysFullDate = this.todaysDate.toLocaleDateString(
       undefined, {
         weekday: 'long', 
         year: 'numeric', 
@@ -20,41 +20,48 @@ class Body extends React.Component {
       }
     );
 
-    // ***  Though the body will always display today's date, I will hard-   ***
-    // ***  code the wordcross's date under the hood. Since I'm not going to    ***
-    // ***  add a new Micro and new Daily wordcross every day like the NYT, I   ***
-    // ***  implement a case statement that determines which of my seven     ***
-    // ***  seeded wordcrosses gets displayed in the body based on today's day   ***
-    // ***  of the week.                                                     ***
+// ***  Though the body will always display today's date, I hardcode   ***
+// ***  the wordcross's date under the hood. Since I'm not going to    ***
+// ***  add new Micro and Daily wordcrosses every day like the NYT, I  ***
+// ***  implement a case statement that determines which of my seven   ***
+// ***  seeded wordcrosses gets displayed in the body based on the     ***
+// ***  current day of the week.                                       ***
 
-    switch (todaysDate.getDay()) {
+    switch (this.todaysDate.getDay()) {
       case 0:
         this.microDate = "2020-08-03";
         // this.dailyDate = "";
+        // this.dailyType = "Sunday";
         break;
       case 1:
         this.microDate = "2020-10-22";
         // this.dailyDate = "";
+        // this.dailyType = "Monday"
         break;
       case 2:
         this.microDate = "2020-10-26";
         // this.dailyDate = "";
+        // this.dailyType = "Tuesday"
         break;
       case 3:
         this.microDate = "2019-09-22";
         // this.dailyDate = "";
+        // this.dailyType = "Wednesday"
         break;
       case 4:
         this.microDate = "2020-10-25";
         // this.dailyDate = "";
+        // this.dailyType = "Thursday"
         break;
       case 5:
         this.microDate = "2020-10-21";
         // this.dailyDate = "";
+        // this.dailyType = "Friday"
         break;
       case 6:
         this.microDate = "2020-08-08";
         // this.dailyDate = "";
+        // this.dailyType = "Saturday"
     }
    
     this.isSubscriber = this.props.currentUser ? "subscriber" : "non-subscriber";
@@ -76,13 +83,16 @@ class Body extends React.Component {
         }
         <Advert isSubscriber={this.isSubscriber} />
         
-        {this.props.microAuthor && 
+        {(this.props.microAuthor || this.props.dailyAuthor) && 
         <Dashboard  
-        microDate = {this.microDate}
-        todaysFullDate = {this.todaysFullDate}
-        isSubscriber = {this.isSubscriber}
-        microAuthor = {this.props.microAuthor}
-        // dailyAuthor = {this.props.dailyAuthor}
+          isSubscriber = {this.isSubscriber}
+          today = {this.todaysDate}
+          todaysFullDate = {this.todaysFullDate}
+          microAuthor = {this.props.microAuthor}
+          microDate = {this.microDate}
+          // dailyAuthor = {this.props.dailyAuthor}
+          // dailyDate = {this.dailyDate}
+          // dailyType = {this.dailyType}
         /> 
       }
       </main>
