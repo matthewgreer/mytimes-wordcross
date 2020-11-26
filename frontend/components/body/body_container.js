6114b3447fce4c98fe6195be0cbf0f63025d278a
matthewgreer <matthew.greer.dev@gmail.com>
2020-11-26 13:52:06 -0500

    Add fetchUserMicro & Daily to body container

--- frontend/components/body/body_container.js
+++ frontend/components/body/body_container.js
@@ -2,19 +2,30 @@ import { connect } from 'react-redux';
 import Body from './body';
 import { fetchMicroAuthor } from "../../actions/micro_actions";
 // import { fetchDailyAuthor } from "../../actions/daily_actions";
+import { fetchUserMicro } from "../../actions/user_micro_actions";
+// import { fetchUserDaily } from "../../actions/user_daily_actions";
 
 const msp = (state) => ({
   currentUser: state.entities.users[state.session.id],
   microAuthor: state.entities.micros.author,
+  microDataSet: state.entities.userMicros.user_micro,
   // dailyAuthor: state.entities.dailies.author
+  // dailyDataSet: state.entities.userDailys.user_daily,
 });
 
 const mdp = (dispatch) => ({
   fetchMicroAuthor: (wordcross_date) => {
-    dispatch(fetchMicroAuthor(wordcross_date))
+    dispatch(fetchMicroAuthor(wordcross_date));
   },
-  // fetchDailyAuthor: (wordcross_date) =>
-    // dispatch(fetchDailyAuthor(wordcross_date))
+  fetchUserMicro: (user_id, wordcross_date) => {
+    dispatch(fetchUserMicro(user_id, wordcross_date));
+  },
+  // fetchDailyAuthor: (wordcross_date) => {
+  // dispatch(fetchDailyAuthor(wordcross_date));
+  // },
+  // fetchUserDaily: (user_id, wordcross_date) => {
+  //   dispatch(fetchUserDaily(user_id, wordcross_date));
+  // }
 });
 
 const BodyContainer = connect(msp, mdp)(Body);
