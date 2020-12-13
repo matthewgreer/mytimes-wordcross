cecc2512ded77cac98340fce8cbcf48ef0940cda
matthewgreer <matthew.greer.dev@gmail.com>
2020-12-12 22:19:08 -0500

    Update routes for Daily

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
  
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create] do 
+      get 'user_dailies/:wordcross_date', to: 'user_dailies#show', as: 'daily_fetch'
+      patch 'user_dailies/:id', to: 'user_dailies#update', as: 'daily_update'
       get 'user_micros/:wordcross_date', to: 'user_micros#show', as: 'micro_fetch'
       patch 'user_micros/:id', to: 'user_micros#update', as: 'micro_update'
     end
+    get 'dailies/:wordcross_date', to: 'dailies#show', as: 'daily_fetch_author'
     get 'micros/:wordcross_date', to: 'micros#show', as: 'micro_fetch_author'
     resource :session, only: [:create, :destroy]
   end
