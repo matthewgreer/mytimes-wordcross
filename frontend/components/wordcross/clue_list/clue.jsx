609e66d55de4f0359ece2090959fef090b87ed3a
matthewgreer <matthew.greer.dev@gmail.com>
2020-12-17 13:18:17 -0500

    Adjust styling

--- frontend/components/wordcross/clue_list/clue.jsx
+++ frontend/components/wordcross/clue_list/clue.jsx
@@ -1,32 +1,5 @@
 import React from 'react';
 
-// const Clue = ({
-//     clueName, 
-//     clue, 
-//     highlight,
-//     number,
-//     handleClueClick
-//   }) => {
-
-//   const handleClick = (e) => {
-//     handleClueClick(clueName);
-//   };  
-
-//   return (
-//     <li
-//       className={`clue-set-clue ${highlight}`}
-//       onClick={handleClick}
-//     > 
-//       <span className="clue-set-clue-number">
-//         {number}
-//       </span>
-//       <span className="clue-set-clue-text">
-//         {clue}
-//       </span>
-//     </li>
-//   );
-// };
-
 class Clue extends React.Component {
   constructor(props) {
     super(props);
