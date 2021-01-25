28f0f53939e83d66850852f3bf7c1f6cd311a755
matthewgreer <matthew.greer.dev@gmail.com>
2021-01-24 19:03:50 -0500

    Clear user puzzles from state upon logout

--- frontend/reducers/user_micros_reducer.js
+++ frontend/reducers/user_micros_reducer.js
@@ -1,8 +1,11 @@
 import { RECEIVE_USER_MICRO } from '../actions/user_micro_actions';
+import { LOGOUT_CURRENT_USER } from '../actions/session_actions';
 
 const userMicrosReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
+    case LOGOUT_CURRENT_USER:
+      return {};
     case RECEIVE_USER_MICRO:
       return Object.assign({}, state, action.user_micro);
     default:
