ee1fc2a4f6f39180fed8b20a37c52735f6735c90
matthewgreer <matthew.greer.dev@gmail.com>
2023-06-15 19:29:49 -0400

    update npm to 7.11.2, webpack to 4.46.0

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,6 @@ module.exports = {
             presets: [
               "@babel/env",
               "@babel/react",
-            //   {
-            //     plugins: ["@babel/plugin-proposal-class-properties"],
-            //   }
             ],
           },
         },
