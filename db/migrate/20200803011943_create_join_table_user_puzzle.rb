ae9368cd0abbd822ec32e63baed6788ec71b68d3
matthewgreer <matthew.greer.dev@gmail.com>
2023-06-18 15:18:07 -0400

    remove redundant ids in join table migration

--- db/migrate/20200803011943_create_join_table_user_puzzle.rb
+++ db/migrate/20200803011943_create_join_table_user_puzzle.rb
@@ -1,8 +1,6 @@
 class CreateJoinTableUserPuzzle < ActiveRecord::Migration[5.2]
   def change
     create_join_table :users, :puzzles do |t|
-      t.integer :user_id, null: false
-      t.integer :puzzle_id, null: false
       t.string :puzzle_state, null: false
       t.datetime :timer_state, null: false
       t.datetime :completion_date_time, null: false
