94f1ce886961b1407aa0ad59311b5b8539179cc5
matthewgreer <matthew.greer.dev@gmail.com>
2023-10-24 15:21:44 -0400

    implement columns instead of ratio in grid, box

--- frontend/components/wordcross/grid/grid.jsx
+++ frontend/components/wordcross/grid/grid.jsx
@@ -4,7 +4,7 @@ import Box from './box';
 const Grid = (
   {
     board,
-    ratio,
+    columns,
     labelSet,
     boxInFocusName,
     activeClueBoxArray,
@@ -20,41 +20,39 @@ const Grid = (
     handleArrowKey
   }
 ) => {
-    return (
+  return (
     <div className="wordcross-grid-container">
-      <div id="wordcross-grid">
-      {board && board.map((row, rowIdx) => {
-        return (
-          <div key={"row" + rowIdx.toString()} className="wordcross-row">
-            {row.map((boxValue, boxIdx) => {
-              const position = [rowIdx, boxIdx];
-              const label = labelSet[rowIdx][boxIdx];
-              const boxName = position.toString();
-              return (
-                <Box
-                  key={boxName}
-                  boxName={boxName}
-                  ratio={ratio}
-                  isBlackBox={boxValue === "#"}
-                  isBoardBlurred={isBoardBlurred}
-                  value={boxValue}
-                  label={label}
-                  isHighlighted={activeClueBoxArray.includes(boxName)}
-                  isInFocus={boxName === boxInFocusName}
-                  changeSolvingDirection={changeSolvingDirection}
-                  handleBoxClick={handleBoxClick}
-                  handleCharacterKey={handleCharacterKey}
-                  handleTabOrEnter={handleTabOrEnter}
-                  handleSpacebar={handleSpacebar}
-                  handleBackspace={handleBackspace}
-                  handleDelete={handleDelete}
-                  handleArrowKey={handleArrowKey}
-                />  
-              );
-            })}
-          </div>
-        )
-      })}
+      <div id="wordcross-grid"
+        style={{ gridTemplateColumns: `repeat(${columns}, 1fr` }}
+      >
+        {board && board.map((row, rowIdx) => {
+          return row.map((boxValue, boxIdx) => {
+            const position = [rowIdx, boxIdx];
+            const label = labelSet[rowIdx][boxIdx];
+            const boxName = position.toString();
+            return (
+              <Box
+                key={boxName}
+                boxName={boxName}
+                columns={columns}
+                isBlackBox={boxValue === "#"}
+                isBoardBlurred={isBoardBlurred}
+                value={boxValue}
+                label={label}
+                isHighlighted={activeClueBoxArray.includes(boxName)}
+                isInFocus={boxName === boxInFocusName}
+                changeSolvingDirection={changeSolvingDirection}
+                handleBoxClick={handleBoxClick}
+                handleCharacterKey={handleCharacterKey}
+                handleTabOrEnter={handleTabOrEnter}
+                handleSpacebar={handleSpacebar}
+                handleBackspace={handleBackspace}
+                handleDelete={handleDelete}
+                handleArrowKey={handleArrowKey}
+              />
+            );
+          });
+        })};
       </div>
     </div>
   );
